e72e6f3e0b254e59783daf49b6b07b6d63ab5847
FernandoR <Fejota001@hotmail.com>
2017-09-30 17:00:21 -0500

    sin terminar

---
+++ app/src/main/java/com/example/android/celulares/metodos.java
@@ -0,0 +1,70 @@
+package com.example.android.celulares;
+
+import java.util.ArrayList;
+
+/**
+ * Created by android on 30/09/2017.
+ */
+
+public class metodos {
+
+    private static ArrayList<celulares> cel = new ArrayList<>();
+
+    public static void Guardarcelulares(celulares p){
+        cel.add(p);
+    }
+
+    public static ArrayList<celulares> Obtener(){
+        return  cel;
+    }
+
+    public static ArrayList<celulares> Obtener_precio_capacidad(ArrayList<celulares> cel)
+    {
+
+        ArrayList<celulares> resultado = new ArrayList<>();
+        for(int i = 0 ; i< cel.size();i++){
+            if (cel.get(i).getMarca()==1 && cel.get(i).getColor()==0 && cel.get(i).getSistema()==0){
+                resultado.add(cel.get(i));
+
+            }
+        }
+
+        return  resultado;
+    }
+
+    public static int Obtener_marca_apple(ArrayList<celulares> cel)
+    {
+        int sw =0 ;
+
+        for(int i = 0 ; i< cel.size();i++){
+            if (cel.get(i).getMarca()==3 && cel.get(i).getColor()==0){
+                sw = sw+1;
+            }
+        }
+
+        return   sw;
+    }
+
+    public static double celulares_precio_nokia(ArrayList<celulares> cel)
+    {
+        double suma = 0;
+        int sw =0 ;
+        if (cel.size()==0){
+            return 0;
+        }
+        for(int i = 0 ; i< cel.size();i++){
+            if (cel.get(i).getMarca()==0){
+                suma = suma + cel.get(i).getPrecio();
+                sw = sw+1;
+            }
+        }
+
+        return  (suma/sw);
+    }
+
+    public static boolean ValidarEnteros(double cap, double pre){
+        if (cap ==0 || pre ==0)return false;
+        return true;
+    }
+}
+
---
+++ app/src/main/java/com/example/android/celulares/reportes.java
@@ -0,0 +1,50 @@
+package com.example.android.celulares;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class reportes extends AppCompatActivity {
+    private String reporte[];
+    private EditText res;
+    private Resources resources;
+    private ArrayList<celulares> celulares;
+    private Spinner reporte_spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportes);
+        resources=this.getResources();
+        celulares = metodos.Obtener();
+        reporte_spinner=(Spinner) findViewById(R.id.cbxreporte);
+        reporte=resources.getStringArray(R.array.reporte);
+        ArrayAdapter<String> adapter_reporte = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,reporte);
+        reporte_spinner.setAdapter(adapter_reporte);
+        res=(EditText)findViewById(R.id.txtresultado);
+    }
+    public void Generar_Reporte(View v){
+        int sel_reporte = reporte_spinner.getSelectedItemPosition();
+        if (sel_reporte==0){
+            ArrayList<celulares> cel = metodos.Obtener_precio_capacidad(celulares);
+            String resultado = "";
+            for (int i = 0; i < cel.size() ; i++) {
+                resultado = resultado +  "celular No." + (i+1) + ": \nPrecio = " + cel.get(i).getPrecio() + ", \nMemoria = " + cel.get(i).getCapacidad() + "\n\n";
+
+            }
+            res.setText(resultado);
+        }else if(sel_reporte==1){
+            res.setText(""+metodos.Obtener_marca_apple(celulares));
+        }else{
+            res.setText(""+metodos.celulares_precio_nokia(celulares));
+        }
+    }
+
+
+
+}
